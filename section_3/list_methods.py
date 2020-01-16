6815a0f699756b14c6d14adc0c152d1ceb046c53
David Sanchez <deividsanchez96@hotmail.com>
2020-01-16 01:36:45 -0500

    Add list methods e.g.

---
+++ section_3/list_methods.py
@@ -0,0 +1,18 @@
+# List methods
+basket = [1, 2, 3, 4, 5]
+
+# adding
+basket.append(100)
+
+# insert
+basket.insert(6, 200)
+
+# extending
+basket.extend([300, 400])
+
+# removing
+basket.pop()
+
+basket.remove(100)
+
+print(basket)
