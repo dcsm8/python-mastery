d967a85ee4978ca836d73e81c14244d35dcb66d8
David Sanchez <deividsanchez96@hotmail.com>
2020-01-17 02:09:34 -0500

    Add dictionary methods e.g.

--- section_3/dictionaries.py
+++ section_3/dictionaries.py
@@ -6,4 +6,24 @@ dictionary = {
     'c': True
 }
 
-print(dictionary['a'])
+user = dict(name='David')
+
+print(dictionary.get('d'))
+print(user)
+
+
+print('a' in dictionary)
+print('hello' in dictionary.values())
+
+user2 = user.copy()
+
+print(user)
+print(user2)
+
+user2.clear()
+
+print(user)
+print(user2)
+
+user.update({'name': 'Dave', 'age': 23})
+print(user)
