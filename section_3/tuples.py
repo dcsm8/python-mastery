49cb932e1396863f11412e0cf19380695794ee80
David Sanchez <deividsanchez96@hotmail.com>
2020-01-17 02:33:08 -0500

    Add tuples e.g.

---
+++ section_3/tuples.py
@@ -0,0 +1,15 @@
+# Tuples
+
+my_tuple = (1, 2, 3, 4, 5)
+
+print(my_tuple)
+
+user = {
+    (1, 2): [1, 2, 3],
+    'greet': 'hello'
+}
+
+print(user[(1, 2)])
+
+print(my_tuple.count(1))
+print(my_tuple.index(4))
