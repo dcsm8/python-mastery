936bff1e6f7c57c31cc4006a5fa345f9c75fdb82
David Sanchez <deividsanchez96@hotmail.com>
2020-01-16 23:00:35 -0500

    Add common list patterns e.g.

---
+++ section_3/common_list_patterns.py
@@ -0,0 +1,12 @@
+basket = ['a', 'x', 'b', 'c', 'd', 'e', 'd']
+
+basket.sort()
+
+print(len(basket))
+
+print(basket[:])
+print(basket[::-1])  # reverse the list and return a new list
+
+print(list(range(100)))
+
+print(' '.join(['Hi', 'my', 'name', 'is', 'David']))
